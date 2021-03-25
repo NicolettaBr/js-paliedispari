52b3a45dbaf650e577fd2677c4a1bc8bcb2b95ff
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-03-25 18:38:39 +0100

    esercizio fatto

---
+++ parole palindrome/js/script.js
@@ -0,0 +1,42 @@
+//Chiedere all’utente di inserire una parola 
+//Creare una funzione per capire se la parola inserita è palindroma
+
+//chiedere una parola all' utente (stringa)
+
+var parolaUtente = prompt('Dimmi una parola');
+//console.log(parolaUtente);
+
+//crea ciclo per leggere parolaUtente da sinistra a destra
+for (var i = 0; i < parolaUtente.length; i++){
+    var parolaDritta = parolaUtente[i];
+    //console.log(parolaDritta);
+}
+
+//crea ciclo per leggere parolaUtente da destra a sinistra
+for (var i = parolaUtente.length; i >= 0; i--){
+    var parolaContraria = parolaUtente[i];
+    //console.log(parolaContraria);
+}
+
+
+//crea funzione se le 2 parole = --> palindroma altrimenti --> non palindroma
+
+function valutaParola (parolaDaValutare){
+    var risultato;
+    
+    //se (paroladritta = parolacontraria) = palindroma
+    //altrimenti no
+    if(parolaDritta == parolaContraria){
+        risultato = 'palindroma';
+    }else{
+        risultato = 'non palindroma';
+    }
+    return risultato;
+}
+
+
+var parolaValutata = valutaParola(parolaUtente);
+console.log(parolaValutata);
+
+//stampa
+alert(parolaValutata);
