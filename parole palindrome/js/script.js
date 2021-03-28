a479e9ff59503d62597521859dd3a0c2c744f94e
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-03-28 16:32:48 +0200

    cicli for inseriti nella funzione

--- parole palindrome/js/script.js
+++ parole palindrome/js/script.js
@@ -4,28 +4,28 @@
 //chiedere una parola all' utente (stringa)
 
 var parolaUtente = prompt('Dimmi una parola');
-//console.log(parolaUtente);
-
-//crea ciclo per leggere parolaUtente da sinistra a destra
-for (var i = 0; i < parolaUtente.length; i++){
-    var parolaDritta = parolaUtente[i];
-    //console.log(parolaDritta);
-}
-
-//crea ciclo per leggere parolaUtente da destra a sinistra
-for (var i = parolaUtente.length; i >= 0; i--){
-    var parolaContraria = parolaUtente[i];
-    //console.log(parolaContraria);
-}
+console.log(parolaUtente);
 
 
 //crea funzione se le 2 parole = --> palindroma altrimenti --> non palindroma
 
 function valutaParola (parolaDaValutare){
+    //crea ciclo per leggere parolaUtente da sinistra a destra
+for (var i = 0; i < parolaUtente.length; i++){
+    var parolaDritta = parolaUtente[i];
+    console.log(parolaDritta);
+    
+}
+
+   //crea ciclo per leggere parolaUtente da destra a sinistra
+for (var i = parolaUtente.length; i >= 0; i--){
+    var parolaContraria = parolaUtente[i];
+    console.log(parolaContraria);
+}
     var risultato;
     
     //se (paroladritta = parolacontraria) = palindroma
-    //altrimenti no
+    //altrimenti no palindroma
     if(parolaDritta == parolaContraria){
         risultato = 'palindroma';
     }else{
